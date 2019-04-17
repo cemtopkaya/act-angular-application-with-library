a2c5b9607271625f335dbb1afaf58e2c268b3d24
cemtopkaya <cem.topkaya@hotmail.com>
2019-04-17 16:26:38 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# mg-ng-company-userlist-masterdetail-v5-mn1xta
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/mg-ng-company-userlist-masterdetail-v5-mn1xta)
---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+import { CompanyModule } from '../../projects/company';
+
+import { DemoMaterialModule } from './demo-module/demo-material.module';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    DemoMaterialModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CompanyModule
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: []
+})
+export class AppModule { }
